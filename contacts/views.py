394eb4f295727b1651081ad90de4a30f53a8de78
iftekhar joy <chowdhuryiftekharulislam@outlook.com>
2019-01-17 20:47:43 +0600

    views modified

--- contacts/views.py
+++ contacts/views.py
@@ -24,9 +24,19 @@ def contact(request):
             if has_contacted:
                 messages.error(request, 'You have already made an inquiry for this listing')
                 return redirect('/listing/'+listing_id)
-        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
         
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
         contact.save()
+
+        # # Send email
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry for '+ listing +'. Sign into the admin panel for more info',
+        #     'ciftekharulislam@gmail.com',
+        #     [realtor_email, 'pythonlove88@gmail.com'],
+        #     fail_silently=False
+
+        # )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
